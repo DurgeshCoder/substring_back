cbe1a486851b06e47b9a79f3feacf9cca55cdebc
batchlcwd <batchlcwd@gmail.com>
2025-08-18 14:53:26 +0530

    docker compose db change

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,6 +9,6 @@ done
 echo "MySQL started"
 
 python manage.py collectstatic --noinput
-#python manage.py migrate --noinput
+python manage.py migrate --noinput
 
 gunicorn substring_back.wsgi:application --bind 0.0.0.0:8000
